ce0fdeeef415ca96bae861cf90b51b6ec7ad89d7
Amara Oussama <45018850+amaraoussama94@users.noreply.github.com>
2019-05-29 13:28:36 +0100

    Add files via upload

--- SendSMS.py
+++ SendSMS.py
@@ -3,31 +3,31 @@ import serial
 import RPi.GPIO as GPIO
 import os , time
 
-
-#init  seriial port com "ttyS0" ou "ttyAMA0 "
+GPIO.setmode(GPIO.BOARD)
+#init  seriial port com "ttyS0" or "ttyAMA0 "
 port=serial.Serial("/dev/ttyS0",baudrate=9600,timeout=1)
 
 #Transmitting AT Commands to the Modem 'r\n' indicates the Enter Key
-
-port.write('AT'+'\r\n')
+#ser.write(str.encode('allon'))   or use ser.write(serialcmd.encode()) else error: unicode strings are not supported, please encode to bytes:
+port.write(str.encode('AT\r\n'))
 rcv=port.read(10)
 print(rcv)
 time.sleep(1)
 
 #Disable the Echo
-port.write('ATE0'+'\r\n')
-rcv=port.read(10)
-print(rcv)
-time.sleep(1)
+#port.write(str.encode('ATE0'+'\r\n'))
+#rcv=port.read(10)
+#print(rcv)
+#time.sleep(1)
 
 #select Message format as TEXT mode
-port.write('AT+CMGF=1'+'\r\n')
+port.write(str.encode('AT+CMGF=1\r\n'))
 rcv=port.read(10)
 print(rcv)
 time.sleep(1)
 
 #New  SMS Message Indications
-port.write('AT+CNMI=2,1,0,0,0'+'\r\n')
+port.write(str.encode('AT+CNMI=2,1,0,0,0\r\n'))
 rcv=port.read(10)
 print(rcv)
 time.sleep(1)
@@ -35,17 +35,17 @@ time.sleep(1)
 
 #Sending a message to a particular Number(chose the Number phone you like to send SMS )
 
-port.write('AT+CMGS="+21625576469"'+'\r\n')
+port.write(str.encode('AT+CMGS="25576469"\r\n'))
 rcv=port.read(10)
 print(rcv)
 
 #Message text
-port.write('Hello User'+'\r\n')
+port.write(str.encode('Hello User\r\n'))
 rcv=port.read(10)
 print(rcv)
 
 #Enable to send SMS = ctrl+z
-port.write("\x1A")
+port.write(str.encode("\x1A"))
 for i in range(10):
     rcv=port.read(10)
     print(rcv)
